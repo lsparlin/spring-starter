93f9f9fbfaaf29812f6da968b2a07d6d1068d1b5
lsparlin <lsparlin@oreillyauto.com>
2014-01-28 14:29:22 -0600

    Added test data and integration test for new CrudRepository

--- src/test/java/rename/repository/impl/HibernateCrudRepositoryTest.java
+++ src/test/java/rename/repository/impl/HibernateCrudRepositoryTest.java
@@ -1,6 +1,7 @@
 package rename.repository.impl;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 import java.util.List;
 
@@ -11,20 +12,45 @@ import org.springframework.transaction.annotation.Transactional;
 import rename.model.RenameMe;
 import rename.repository.CrudRepository;
 
+/*
+ * This test assumes CreateTestData.java inserted some records when the context was built 
+ */
+
 @Transactional
 public class HibernateCrudRepositoryTest extends AbstractRepositoryTest {
 
     @Autowired
-    CrudRepository genericRepo;
+    CrudRepository crudRepo;
 
     @Test
-    public void entity_is_saved_and_retreived() {
+    public void testGetById() {
+        RenameMe renameMe = crudRepo.getById(RenameMe.class, 1L);
+        assertNotNull(renameMe);
+        assertEquals("Name 1", renameMe.getName());
+    }
+
+    @Test
+    public void testFindAll() {
+        List<RenameMe> list = crudRepo.findAll(RenameMe.class);
+        assertEquals(5, list.size());
+    }
+
+    @Test
+    public void testDelete() {
+        int count = crudRepo.findAll(RenameMe.class).size();
+        RenameMe renameMe = crudRepo.getById(RenameMe.class, 1L);
+        crudRepo.delete(renameMe);
+        assertEquals(count - 1, crudRepo.findAll(RenameMe.class).size());
+    }
+
+    @Test
+    public void testSave() {
+        int count = crudRepo.findAll(RenameMe.class).size();
         RenameMe renameMe = new RenameMe();
-        renameMe.setName("Name1");
-        genericRepo.save(renameMe);
-        List<RenameMe> list = genericRepo.findAll(RenameMe.class);
-        assertEquals(1, list.size());
-        assertEquals("Name1", list.get(0).getName());
+        renameMe.setName("New Name");
+        crudRepo.save(renameMe);
+
+        assertEquals(count + 1, crudRepo.findAll(RenameMe.class).size());
     }
 
 }
